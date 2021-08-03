99f58dd788c270872665e8ce72205d115ac635b6
Vikas Dubey <vikasdubeysu9090@gmail.com>
2021-08-03 16:07:16 +0530

    quize app Quizee

---
+++ questions.js
@@ -0,0 +1,233 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question:"The files that can be committed are always present in git ",
+    answer: "staging area",
+    options: [
+      "working directory",
+      "staging area",
+      "unstaged area",
+      "Anywhere, there is no barrier"
+    ]
+  },
+  {
+    numb: 6,
+    question:"What is the time complexity of searching for an element in a circular linked list?",
+
+    answer: "O(n)",
+    options: [
+      "O(n)",
+      "O(nlogn)",
+      "O(1)",
+      "O(n2)"
+      
+    ]
+  },
+  {
+    numb: 6,
+    question:" Which of these best describes an array?",
+    answer: "Container of objects of similar types",
+    options: [  
+
+     "A data structure that shows a hierarchical behavior",
+    "Container of objects of similar types",
+    "Arrays are immutable once initialised",
+    "Array is not a data structure"
+
+    
+    ]
+  },
+  {
+    numb: 6,
+    question:"Git can be used for which project?",
+    answer: "All of the above",
+    options: [
+      "Java enterprise project",
+      ".Net project",
+      "File version management",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question:"How do you initialize an array in C?",
+
+    answer: " int arr[3] = {1,2,3};",
+    options: [
+      "int arr[3] = (1,2,3);",
+    "int arr(3) = {1,2,3};",
+    " int arr[3] = {1,2,3};",
+    "int arr(3) = (1,2,3);"
+    
+    ]
+  },
+  {
+    numb: 6,
+    question:" How do you instantiate an array in Java?",
+    answer: "int arr[] = new int[3];",
+    options: [
+      "int arr[] = new int(3);",
+    "int arr[];",
+    "int arr[] = new int[3];",
+    "int arr() = new int(3);"
+    ]
+  },
+  {
+    numb: 6,
+    question:"Which of the following is true about Centralized version control system?",
+    answer: "Both B & C",
+    options: [
+    "It does not rely on the central server",
+    "It is victim to single point of failure",
+    "It rely on the central server",
+    "Both B & C"
+    ]
+  },
+  {
+    numb: 6,
+    question:"What is full form of VCS?",
+    answer: "Version Control System",
+    options: [
+      "Version Configuration System",
+    "Version Consolidated Solutions",
+    "Version Configuration Solutions",
+    "Version Control System"
+    ]
+  },
+  {
+    numb: 6,
+    question:"Assuming int is of 4bytes, what is the size of int arr[15]",
+    answer: "60",
+    options: [
+    "13",
+    "60",
+    "4",
+    "20"
+    ]
+
+  },
+  {
+    numb: 6,
+    question:"Git is a __________",
+    answer: "Distributed version control system",
+    options: [
+      "Distributed version control system",
+      "Centralized version control system",
+      "Localized version control system",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 6,
+    question:"Which of the following application makes use of a circular linked list?",
+    answer: "Allocating CPU to resources",
+    options: [
+      "Undo operation in a text editor",
+      "Recursive function calls",
+      "Allocating CPU to resources",
+      "Implement Hash Tables"
+    ]
+  },
+  {
+    numb: 6,
+    question:"What differentiates a circular linked list from a normal linked list?",
+    answer: "You may or may not have the ‘next’ pointer point to null in a circular linked list",
+    options: [
+      "What differentiates a circular linked list from a normal linked list?",
+      "It is faster to traverse the circular linked lis",
+      "Head node is known in circular linked list",
+      "You may or may not have the ‘next’ pointer point to null in a circular linked list"
+    ]
+  },
+  {
+    numb: 6,
+    question:"Which of the following usees FIFO method",
+    answer: "Queue",
+    options: [
+      
+    "Queue",
+    "Stack",
+    "Hash Table",
+  "Binary Search Tree"
+    ]
+  },
+  {
+    numb: 6,
+    question:"GIT comes from",
+    answer: "2005",
+    options: [
+      "2005",
+      "2007",
+      "2003",
+      "2001"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the language is used in GIT?",
+    answer:"C",
+    options: [
+      "C",
+    "Html",
+    "Java",
+    "python"
+    ]
+  },
+];
